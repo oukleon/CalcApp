2de702c4c56ec5ab3f523b386ae1696cb80fb101
oukleon <54334941+oukleon@users.noreply.github.com>
2022-09-06 19:12:10 +0900

    BasicDesigning

---
+++ app/src/main/java/com/example/calcapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.calcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun onDigit(view: View){
+        Toast.makeText(this,"its digit!",Toast.LENGTH_LONG).show()
+    }
+}
